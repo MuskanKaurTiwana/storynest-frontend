d72135d927848d2d4bf8fbe99d97b5af31156cf2
MuskanKaurTiwana <158174452+MuskanKaurTiwana@users.noreply.github.com>
2025-07-25 02:20:00 +0530

    resolve token issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://storynest-backend-hthr.onrender.com/api',
-  withCredentials: true, // Important for sending cookies (for auth)
+});
+
+// Automatically attach token to every request
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
 });
 
 export default API;
